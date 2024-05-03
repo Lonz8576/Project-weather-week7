8790060f5756cbc01077df1506ee4c9fdcabc49d
Lonz8576 <Lonz8576@users.noreply.github.com>
2024-05-03 11:57:25 -0400

    fixed html
    multiple classes and id for js function api to change inner html

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 
+function searchCity(city) {
+    let apiKey = "2e99ddt6a7e37f7c164ob09d070ab380";
+    let apiUrl = "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial";
+console.log(apiUrl);
+
+
+}
+
+
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#city-input");
    let cityElement = document.querySelector("#main-city");
+
    cityElement.innerHTML = searchInput.value;
+   searchCity(searchInput.value);
 }
 
 
