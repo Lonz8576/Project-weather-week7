baecc47d8a39871c65ff2948d3175b9616ff5840
Lonz8576 <Lonz8576@users.noreply.github.com>
2024-05-03 14:23:37 -0400

    JS upated
    Change temp and city when searched, auto response on page is SF CURRENT weather using API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#main-temp");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#main-city");
+
+   cityElement.innerHTML = response.data.city;
+
+    temperatureElement.innerHTML = Math.round(temperature);
+
+
+}
+
+
 function searchCity(city) {
     let apiKey = "2e99ddt6a7e37f7c164ob09d070ab380";
-    let apiUrl = "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial";
-console.log(apiUrl);
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+    axios.get(apiUrl).then(refreshWeather);
 
 
 }
@@ -12,9 +25,7 @@ console.log(apiUrl);
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#city-input");
-   let cityElement = document.querySelector("#main-city");
-
-   cityElement.innerHTML = searchInput.value;
+  
    searchCity(searchInput.value);
 }
 
@@ -23,3 +34,5 @@ function handleSearchSubmit(event) {
 let searchBarElement = document.querySelector("#search-bar");
     searchBarElement.addEventListener("submit", handleSearchSubmit);
     
+    
+    searchCity("San Fransisco");
